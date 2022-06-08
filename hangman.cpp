fb60e19ee72562d9f276f8257f64dc0e4e531da9
Phawn <xplo1t.porlas@gmail.com>
2022-06-08 21:57:48 +0800

    Updated

--- hangman.cpp
+++ hangman.cpp
@@ -8,6 +8,9 @@ void PrintMessage(string message, bool printTop = true, bool printBottom = true)
         cout << "+================================+" << endl;
         cout << "|";
     }
+    else {
+        cout << "|";
+    }
     for (int i = message.length(); i < 33; i++ ){
         if (front){
             message = " " + message;
@@ -22,10 +25,110 @@ void PrintMessage(string message, bool printTop = true, bool printBottom = true)
         cout << "|" << endl ;
         cout << "+================================+" << endl;
     }
+    else {
+        cout << "|" << endl;
+    }
+}
+void DrawHangman(int guessCount = 0){
+    if (guessCount >= 1){
+        PrintMessage("|", false, false);
+    }
+    else{
+        PrintMessage("", false, false);
+    }
+
+    if (guessCount >= 2){
+        PrintMessage("|", false, false);
+    }
+    else{
+        PrintMessage("", false, false);
+    }
+
+    if (guessCount >= 3){
+        PrintMessage("O", false, false);
+    }
+    else{
+        PrintMessage("", false, false);
+    }
+
+    if (guessCount == 4){
+        PrintMessage("/  ", false, false);
+    }
+    
+    if (guessCount == 5){
+        PrintMessage("/| ", false, false);
+    }
+
+    if (guessCount >= 6){
+        PrintMessage("/|\\", false, false);
+    }
+    
+    if (guessCount >= 7){
+        PrintMessage("|", false, false);
+    }
+    else{
+        PrintMessage("", false, false);
+    }
+
+    if (guessCount == 8){
+        PrintMessage("/  ", false, false);
+    }
+
+    if (guessCount >= 9){
+        PrintMessage("/ \\", false, false);
+    }
+    
+    PrintMessage("+===========+", false, false);
+    PrintMessage("|           |", false, false);
+}
+void PrintLetters(string input, char from, char to){
+    string s;
+    for (char i = from; i <= to; i++){
+        if (input.find(i) == string::npos){
+            s += i;
+            s += " ";
+        }
+        else {
+            s += " ";
+        }
+    }
+    PrintMessage(s, false, false);
+}
+void PrintAvailableLetters(string taken){
+    PrintMessage("_________________________________", false, false);
+    PrintMessage(" ", false, false);
+    PrintLetters(taken, 'A', 'M');
+    PrintLetters(taken, 'N', 'Z');
+    PrintMessage("_________________________________", false, false);
+}
+bool PrintWordAndCheckWin(string word, string guessed){
+    string s;
+    int wl = word.length();
+    bool won = true;
+
+    for (int i = 0; i < wl; i++){
+        if (guessed.find(word[i]) == string::npos){
+            won = false;
+            s += "_ ";
+        }
+        else {
+            s += word[i];
+            s += " ";
+        }
+    }
+    PrintMessage("_________________________________", false, false);
+    PrintMessage(s, false);
+
+    return won;
 }
 
 int main(){
+    string guesses = "PSQUARED";
     PrintMessage("HANG MAN");
+    DrawHangman(0);
+    PrintAvailableLetters(guesses);
+    PrintMessage("[CLUE STATEMENT]", false, false);
+    PrintWordAndCheckWin("PAOPE", guesses);
     return 0;
 }
 
